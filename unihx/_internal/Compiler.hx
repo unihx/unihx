7a46d989443232118bff83abeec183afb3037bda
Cauê Waneck <waneck@gmail.com>
2014-05-13 03:17:41 -0300

    working correctly

--- unihx/_internal/Compiler.hx
+++ unihx/_internal/Compiler.hx
@@ -8,6 +8,8 @@ class Compiler
 	macro public static function compile():Void
 	{
 		var cwd = fullPath(haxe.io.Path.removeTrailingSlashes(Sys.getCwd()));
+		trace(cwd);
+		trace(Sys.systemName());
 		// var files = [];
 		for (cp in Context.getClassPath())
 		{
@@ -46,7 +48,27 @@ class Compiler
 				pack.pop();
 			} else if (file.endsWith('.hx')) {
 				// files.push(pack.join('.') + dot + file.substr(0,-3));
-				Context.getModule(pack.join('.') + dot + file.substr(0,-3));
+				var r = sys.io.File.read(path);
+				var hasPack = false;
+				try
+				{
+					while(true)
+					{
+						var ln = r.readLine();
+						var idx = ln.indexOf('package');
+						if (idx >= 0)
+						{
+							hasPack = true;
+							var p = ln.substring(idx + 7, ln.indexOf(';',idx)).trim();
+							if (p == pack.join('.'))
+								Context.getModule(p + dot + file.substr(0,-3));
+							break;
+						}
+					}
+				}
+				catch(e:haxe.io.Eof) {}
+				if (!hasPack && pack.length == 0)
+					Context.getModule(pack.join('.') + dot + file.substr(0,-3));
 			}
 		}
 	}
--- unihx/_internal/editor/AssetProcessor.hx
+++ unihx/_internal/editor/AssetProcessor.hx
@@ -13,7 +13,8 @@ using StringTools;
 			movedAssets:Vector<String>,
 			movedFromAssetPaths:Vector<String>)
 	{
-		var sources = [];
+		var sources = [],
+				refresh = false;
 		for (str in importedAssets)
 		{
 			if (str.endsWith(".hx"))
@@ -34,6 +35,11 @@ using StringTools;
 				if (exists( path ))
 				{
 					deleteFile(path);
+					if (exists( path + '.meta' ))
+						deleteFile( path + '.meta' );
+					if (readDirectory( Path.directory(d) + '/hx-compiled' ).length == 0)
+						deleteDirectory( Path.directory(d) + '/hx-compiled' );
+					refresh = true;
 				}
 			}
 		}
@@ -46,19 +52,29 @@ using StringTools;
 				if (exists( path ))
 				{
 					deleteFile(path);
+					if (exists( path + '.meta' ))
+						deleteFile( path + '.meta' );
+					trace( readDirectory( Path.directory(d) + '/hx-compiled' ) );
+					if (readDirectory( Path.directory(d) + '/hx-compiled' ).length == 0)
+						deleteDirectory( Path.directory(d) + '/hx-compiled' );
+					refresh = true;
 				}
 			}
 		}
 		if (sources.length > 0)
 		{
 			trace("calling");
+			refresh = true;
 			var cmd = new sys.io.Process('haxe',['--cwd',Sys.getCwd() + '/Assets','classpaths.hxml','params.hxml','--macro','unihx._internal.Compiler.compile()']);
 			trace(cmd.exitCode());
 			trace(cmd.stdout.readAll());
 			trace(cmd.stderr.readAll());
-			unityeditor.AssetDatabase.Refresh();
 			// var r = Sys.command('haxe',['--cwd','Assets','classpaths.hxml','params.hxml','--macro','unihx._internal.Compiler.compile\\(\\)']);
 			// trace(r);
+		}
+		if (refresh)
+		{
+			unityeditor.AssetDatabase.Refresh();
 		}
 	}
 }
