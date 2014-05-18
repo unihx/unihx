9b45a64c6d3d75d3bb3657ec0d10a30021016ec7
Cauê Waneck <waneck@gmail.com>
2014-05-17 23:44:13 -0300

    force recompile working correctly

--- unihx/_internal/editor/HaxeCompiler.hx
+++ unihx/_internal/editor/HaxeCompiler.hx
@@ -1,6 +1,10 @@
 package unihx._internal.editor;
 import unihx._internal.editor.HaxeProperties;
 import sys.io.Process;
+import unityengine.*;
+import Std.*;
+
+using StringTools;
 
 class HaxeCompiler
 {
@@ -33,23 +37,55 @@ class HaxeCompiler
 		process = new Process('haxe',['--wait',port + ""]);
 	}
 
-	public function compile(args:Array<String>):Null<sys.io.Process>
+	public function compile(args:Array<String>, verbose=false):Bool
 	{
-		switch(props)
+		var cmd = switch(props)
 		{
 			case DontCompile:
-				return null;
+				return true;
 			case Compile:
 				trace('compile',args);
-				return new Process("haxe",args);
+				new Process("haxe",args);
 			case CompilationServer(port):
 				if (process == null || ( untyped process.native : cs.system.diagnostics.Process ).HasExited )
 					newProcess(port);
 				args = args.copy();
 				args.push('--connect'); args.push(port+"");
 				trace('compile',args);
-				return new Process('haxe',args);
+				new Process('haxe',args);
 		}
+
+		var ret = true;
+		if (cmd != null)
+		{
+			var sw = new cs.system.diagnostics.Stopwatch();
+			sw.Start();
+			if (cmd.exitCode() != 0)
+			{
+				ret = false;
+				Debug.LogError("Haxe compilation failed.");
+			}
+			sw.Stop();
+			if (verbose)
+				Debug.Log('Compilation ended (' + sw.Elapsed + ")" );
+			for (ln in cmd.stdout.readAll().toString().trim().split('\n'))
+			{
+				var ln = ln.trim();
+				if (ln != "")
+					Debug.Log(ln);
+			}
+			for (ln in cmd.stderr.readAll().toString().trim().split('\n'))
+			{
+				var ln = ln.trim();
+
+				if (ln == "") continue;
+				if (ln.startsWith('Warning'))
+					Debug.LogWarning(ln);
+				else
+					reportError(ln);
+			}
+		}
+		return ret;
 	}
 
 	public function close()
@@ -60,5 +96,50 @@ class HaxeCompiler
 			process.close();
 			process = null;
 		}
+	}
+
+	public static function reportError(line:String)
+	{
+		var ln = line.split(':');
+		ln.reverse();
+		var file = ln.pop(),
+				lineno = parseInt(ln.pop()),
+				other = ln.pop(),
+				rest = ln.join(":");
+
+		var fullp = cs.system.io.Path.GetFullPath(cs.system.io.Path.Combine("Assets",file));
+		var debug = cs.Lib.toNativeType(Debug);
+		try
+		{
+			Debug.LogException(new HaxeError(line,fullp,lineno));
+		}
+		catch(e:Dynamic)
+		{
+			Debug.LogError(line);
+		}
+	}
+}
+
+@:nativeGen class HaxeError extends cs.system.Exception
+{
+	var file:String;
+	var line:Int;
+	var msg:String;
+	public function new(msg:String,file:String,line:Int)
+	{
+		super(msg);
+		this.msg = msg;
+		this.file = file;
+		this.line = line;
+	}
+
+	@:overload override private function get_Message():String
+	{
+		return msg;
+	}
+
+	@:overload override private function get_StackTrace():String
+	{
+		return "(at " + file + ":" + line + ")";
 	}
 }
