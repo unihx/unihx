df7ef8b9c7bb819b255d9bdf75e31c7d33d81b82
Cauê Waneck <waneck@gmail.com>
2014-09-28 09:15:33 -0300

    example project window extension

---
+++ unihx/_internal/editor/ExampleProjectWindowExt.hx
@@ -0,0 +1,24 @@
+package unihx._internal.editor;
+import unityengine.*;
+import unityeditor.*;
+
+@:meta(UnityEditor.InitializeOnLoad)
+@:nativeGen @:keep class ExampleProjectWindowExt
+{
+	// static function __init__()
+	// {
+	// 	unityeditor.EditorApplication.projectWindowItemOnGUI += Draw;
+	// }
+
+	// static function Draw(guid:String, rect:Rect):Void
+	// {
+	// 	var path = AssetDatabase.GUIDToAssetPath(guid);
+
+	// 	rect.x += 15;
+	// 	rect.width -= 15;
+	// 	rect.y++;
+
+	// 	var elts = path.split('/');
+	// 	GUI.Label(rect, elts[elts.length - 1], EditorStyles.label);
+	// }
+}
