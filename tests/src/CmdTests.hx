34a7ecd76c759bda9b2343e3ebdaa46bb0636ae4
Cauê Waneck <waneck@gmail.com>
2014-10-02 17:48:06 -0300

    force trace

--- tests/src/CmdTests.hx
+++ tests/src/CmdTests.hx
@@ -5,6 +5,19 @@ class CmdTests
 {
 	static function main()
 	{
+		haxe.Log.trace = function(v:Dynamic, ?infos:haxe.PosInfos) {
+			var str:String = null;
+			if (infos != null) {
+				str = infos.fileName + ":" + infos.lineNumber + ": " + v;
+				if (infos.customParams != null)
+				{
+					str += "," + infos.customParams.join(",");
+				}
+			} else {
+				str = v;
+			}
+			cs.system.Console.WriteLine(str);
+		};
 		var runner = new Runner();
 		runner.addCase( new YieldTests() );
 		runner.run();
