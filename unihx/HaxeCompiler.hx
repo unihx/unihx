470ab828321f6238d67610a1bd88a564c76f3f64
Cauê Waneck <waneck@gmail.com>
2014-10-03 20:15:38 -0300

    sync

---
+++ unihx/HaxeCompiler.hx
@@ -0,0 +1,5 @@
+package unihx;
+
+class HaxeCompiler
+{
+}
---
+++ unihx/_internal/HaxeCompiler.hx
@@ -0,0 +1,14 @@
+package unihx._internal;
+
+class HaxeCompiler
+{
+
+	public function new()
+	{
+	}
+
+	static function main()
+	{
+	}
+
+}
