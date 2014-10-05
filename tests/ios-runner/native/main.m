bf3744895443ae56a1b540120aa38dd0e0f438f8
Cauê Waneck <waneck@gmail.com>
2014-10-04 23:08:41 -0300

    simple command-line app working

--- tests/ios-runner/native/main.m
+++ tests/ios-runner/native/main.m
@@ -1,6 +1,8 @@
+#import <CoreFoundation/CoreFoundation.h>
 #import <UIKit/UIKit.h>
-#include <CoreFoundation/CoreFoundation.h>
+/* #include <CoreFoundation/CoreFoundation.h> */
 #include <stdio.h>
+#include <dlfcn.h>
 
 const char *hxRunLibrary();
 void hxcpp_set_top_of_stack();
@@ -10,11 +12,91 @@ void hxcpp_set_top_of_stack();
 /* extern CFStringRef SBSApplicationLaunchingErrorString(int error); */
 /* #endif */
 
+static int (*pvt_launchApplication)(CFStringRef, Boolean);
+static CFStringRef (*pvt_errorString)(int);
+static void (*pvt_setBacklight)(float);
+
+static char * MYCFStringCopyUTF8String(CFStringRef aString) {
+	if (aString == NULL) {
+		return NULL;
+	}
+
+	CFIndex length = CFStringGetLength(aString);
+	CFIndex maxSize =
+		CFStringGetMaximumSizeForEncoding(length,
+				kCFStringEncodingUTF8);
+	char *buffer = (char *)malloc(maxSize);
+	if (CFStringGetCString(aString, buffer, maxSize, kCFStringEncodingUTF8)) 
+	{
+		return buffer;
+	}
+
+	return NULL;
+}
+
+void mainSetScreenDim(float val)
+{
+	if (NULL != pvt_setBacklight)
+	{
+		pvt_setBacklight(val);
+	} else {
+		printf("set screen dim not available!\n");
+	}
+}
+
+char *mainLaunchApplication(char *name, int suspended)
+{
+	@autoreleasepool { 
+		int ret;
+		char *error = NULL;
+
+		CFStringRef identifier = CFStringCreateWithCString(kCFAllocatorDefault, name, kCFStringEncodingUTF8);
+		if (identifier == NULL)
+		{
+			return NULL;
+		}
+
+		ret = pvt_launchApplication(identifier,suspended);
+		if (ret != 0)
+		{
+			if (NULL != pvt_errorString)
+			{
+				error = MYCFStringCopyUTF8String( pvt_errorString(ret) );
+			} else {
+				const char *msg = "Unkown error occurred. Returned: ";
+				char num[25];
+				snprintf(num, 24, "%d", ret);
+				error = malloc( strlen(msg) + 25);
+				strcpy(error,msg);
+				strcat(error,num);
+			}
+		}
+		CFRelease(identifier);
+
+		return error;
+	}
+}
+
 int main(int argc, char **argv, char **envp)
 {
-		int ret = 0;
+		char *ret = 0;
 		const char *err = NULL;
+		void *hndl;
 		hxcpp_set_top_of_stack();
+
+		// Dynamic loading of private API
+		hndl = dlopen("/System/Library/PrivateFrameworks/SpringBoardServices.framework/SpringBoardServices", RTLD_LAZY);
+		assert(hndl != NULL);
+		pvt_launchApplication = dlsym(hndl, "SBSLaunchApplicationWithIdentifier");
+		assert(pvt_launchApplication != NULL);
+		pvt_errorString = dlsym(hndl, "SBSApplicationLaunchingErrorString");
+		dlclose(hndl);
+		hndl = dlopen("/System/Library/PrivateFrameworks/GraphicsServices.framework/GraphicsServices",RTLD_LAZY);
+		if (NULL != hndl)
+		{
+			pvt_setBacklight = dlsym(hndl, "GSEventSetBacklightLevel");
+			dlclose(hndl);
+		}
 
 		err = hxRunLibrary();
 		if (err) {
@@ -26,19 +108,15 @@ int main(int argc, char **argv, char **envp)
         return -1;
     }
 
-		/* [UIApplication launchApplicationWithIdentifier:[NSString stringWithUTF8String:argv[1]] suspended:NO]; */
+		ret = mainLaunchApplication(argv[1], FALSE);
 
-    /* CFStringRef identifier = CFStringCreateWithCString(kCFAllocatorDefault, argv[1], kCFStringEncodingUTF8); */
-    /* assert(identifier != NULL); */
+    if (ret != NULL) 
+		{
+        fprintf(stderr, "Couldn't open application: %s. Reason: %s\n", argv[1], ret);
+				free(ret);
+				return -1;
+    }
 
-    /* ret = SBSLaunchApplicationWithIdentifier(identifier, FALSE); */
-
-    /* if (ret != 0) { */
-    /*     fprintf(stderr, "Couldn't open application: %s. Reason: %i, ", argv[1], ret); */
-    /*     CFShow(SBSApplicationLaunchingErrorString(ret)); */
-    /* } */
-
-    /* CFRelease(identifier); */
-
-    return ret;
+    return 0;
 }
+
