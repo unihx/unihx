ffa9a8c8f209417eb04c235d1857b84dc83e055d
Cauê Waneck <waneck@gmail.com>
2014-10-06 13:34:15 -0300

    try to reduce the amount of noise

--- tests/run-travis.sh
+++ tests/run-travis.sh
@@ -29,20 +29,22 @@ case ${DEVICE} in
 	ios )
 		cd ~
 		# download example ios app project
-		retry curl -L "http://waneck-pub.s3-website-us-east-1.amazonaws.com/unitdeps/unity/proj-1.tgz" -o proj-1.tgz || exit 1
+		retry curl -L "http://waneck-pub.s3-website-us-east-1.amazonaws.com/unitdeps/unity/proj-1.tgz" -o proj-1.tgz > /dev/null || exit 1
 		tar -zxf proj-1.tgz || exit 1
 		# download ios tools
-		retry curl -L "https://docs.google.com/uc?export=download&id=0B8FjDKR0nfqoU1p1dktXanI4M3c" -o iostools.tgz || exit 1
+		retry curl -L "https://docs.google.com/uc?export=download&id=0B8FjDKR0nfqoU1p1dktXanI4M3c" -o iostools.tgz > /dev/null || exit 1
 		tar -zxf iostools.tgz || exit 1
 		chmod +x iostools/ldid
 		chmod +x iostools/Tools/OSX/mono-xcompiler
 
 		# build ios-runner
 		cd $ORIGINAL/ios-runner
+		echo "Client build"
 		haxe build-client.hxml || exit 1
 		cp bin/client.n ~/ios-remote.n
 
 		cd $ORIGINAL
+		echo "Unity DLL build"
 		# build as we would for unity example app
 		haxe build-unity.hxml || exit 1
 
@@ -55,13 +57,15 @@ case ${DEVICE} in
 		# xcompile
 		cd ~/proj-1/Data/Managed
 		for file in *.dll; do
-			~/iostools/Tools/OSX/mono-xcompiler --aot=full,asmonly,nodebug,static,outfile=$file.s $file || exit 1
+			echo "AOT compiling $file"
+			~/iostools/Tools/OSX/mono-xcompiler --aot=full,asmonly,nodebug,static,outfile=$file.s $file > /tmp/mono-out.out || (cat /tmp/mono-out; exit 1)
 		done
 		mv *.s ../../Libraries || exit 1
 
 		# build using xcode but do not sign
 		cd ../../
-		xcodebuild -alltargets -configuration Debug clean build CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO || exit 1
+		echo "XCode compiling"
+		xcodebuild -alltargets -configuration Debug clean build CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO > /tmp/xcode-out.out || (cat /tmp/xcode-out.out; exit 1)
 		# fake sign
 		~/iostools/ldid -S build/ProductName.app/ProductName || exit 1
 
@@ -70,6 +74,7 @@ case ${DEVICE} in
 		cd build
 		tar -zcf ios-app.tar.gz ProductName.app
 
+		echo "Remote iOs test"
 		neko ~/ios-remote.n --retries 5 -s "$IOS_REMOTE_SHARED" -c "$ORIGINAL/config.json" -f 'ios-app.tar.gz' --connect $IOS_REMOTE_ARGS || exit 1
 		;;
 esac
