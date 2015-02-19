097766984275c43efd3c41cc1d13811421c42eb2
Cauê Waneck <waneck@gmail.com>
2015-02-19 14:35:56 -0200

    force 'always' for haxelib

--- src/unihx/pvt/compiler/Utils.hx
+++ src/unihx/pvt/compiler/Utils.hx
@@ -12,6 +12,8 @@ class Utils
 	public static function runProcess(name:String, args:Array<String>):{ exit:Int, out:String, err:String }
 	{
 		var proc = new sys.io.Process(name,args);
+		proc.stdin.writeString('a\n'); //for haxelib - force always
+		proc.stdin.flush();
 		var err = null;
 #if cs
 		new cs.system.threading.Thread(function() err = proc.stderr.readAll().toString()).Start();
