04ea68dea65a32b8cf7753d81f71d732c9e6ca1f
Cauê Waneck <waneck@gmail.com>
2015-02-23 16:37:16 -0300

    build

---
+++ src/unihx/pvt/macros/HaxeBehaviourBuild.hx
@@ -0,0 +1,15 @@
+package unihx.pvt.macros;
+
+class HaxeBehaviourBuild
+{
+	public static function build()
+	{
+#if editor
+		// build inspector if -D editor
+#end
+		// build serializer if needed
+		// build yield fields if needed
+
+		return null;
+	}
+}
